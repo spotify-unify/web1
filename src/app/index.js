ebabc33d626a9eeb91fe4c305196aa0489a8d600
Carl Englund <englund92@gmail.com>
2015-05-16 19:43:41 +0200

    Can now load tracks from spotify using angular-spotify

--- src/app/index.js
+++ src/app/index.js
@@ -3,18 +3,20 @@
 import MainCtrl from './main/main.controller';
 import NavbarCtrl from '../app/components/navbar/navbar.controller';
 
-angular.module('methuselah', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'mgcrea.ngStrap'])
+angular.module('methuselah', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'mgcrea.ngStrap', 'spotify'])
   .controller('MainCtrl', MainCtrl)
   .controller('NavbarCtrl', NavbarCtrl)
 
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, SpotifyProvider) {
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       });
-
+      SpotifyProvider.setClientId('d74a81e92d4c401cac467f4818a31b82');
+      SpotifyProvider.setRedirectUri('http://localhost:3000');
+      SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public');
     $urlRouterProvider.otherwise('/');
   })
 ;
