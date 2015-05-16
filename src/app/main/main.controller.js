5c17ab1d2e5cd4f3b3c56e04e3a9c70d23e20e77
Caroline Lindström <car.lindstrom@gmail.com>
2015-05-16 22:27:08 +0200

    Put back the spotify play button

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -10,6 +10,7 @@ class MainCtrl {
 
       Spotify.getTracks('0eGsygTp906u18L0Oimnem,1lDWb6b6ieDQ2xT7ewTC3G').then(function (data) {
       $scope.tracks = data.tracks;
+      $scope.playlist = "https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:"+"5Z7ygHQo02SUrFmcgpwsKW,1x6ACsKV4UdWS2FMuPFUiT,4bi73jCM02fMpkI11Lqmfe";
       });
     });
   }
