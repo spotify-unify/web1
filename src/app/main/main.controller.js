fbd5232b8a47fa4a2b0374422773456da9373d2d
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-05-17 10:39:07 +0200

    Add playlist controller, view and route

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,21 +2,11 @@
 
 class MainCtrl {
 
-  constructor ($scope, Spotify, EchonestService) {
+  constructor ($scope) {
 
-    EchonestService.getPlaylistSongs('stockholm').then(function (songsIds) {
-      var url = "https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:";
-      $scope.playlist = url + songsIds.join(',');
-      Spotify.getTracks(songsIds.join(',')).then(function (data) {
-        $scope.tracks = data.tracks;
-        for (var i = 0; i < $scope.tracks.length; i++) {
-          $scope.tracks[i].embeddlink = 'https://embed.spotify.com/?uri=' + $scope.tracks[i].uri;
-        }
-      });
-    });
   }
 }
 
-MainCtrl.$inject = ['$scope', 'Spotify', 'EchonestService'];
+MainCtrl.$inject = ['$scope'];
 
 export default MainCtrl;
