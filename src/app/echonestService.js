4971588bcc96d6acc5982fb53189afc1b70b506c
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-05-17 13:00:27 +0200

    Two space indentation

--- src/app/echonestService.js
+++ src/app/echonestService.js
@@ -1,45 +1,45 @@
 'use strict';
 
 class EchonestService {
-    constructor(Echonest, Spotify, $q) {
-        this.Echonest = Echonest;
-        this.q = $q;
-        this.Spotify = Spotify;
-    }
-    
-    songByArtist(artist_id) {
-        return this.Echonest.songs.search({
-            'artist_id': artist_id,
-            'results': 1,
-            'sort': 'song_hotttnesss-desc',
-            'bucket': ['tracks', 'id:spotify']
-        });
-    }
+  constructor(Echonest, Spotify, $q) {
+    this.Echonest = Echonest;
+    this.q = $q;
+    this.Spotify = Spotify;
+  }
+  
+  songByArtist(artist_id) {
+    return this.Echonest.songs.search({
+      'artist_id': artist_id,
+      'results': 1,
+      'sort': 'song_hotttnesss-desc',
+      'bucket': ['tracks', 'id:spotify']
+    });
+  }
 
-    artistsByLocation(loc, number) {
-        return this.Echonest.artists.search({
-            artist_location: loc,
-            results: number,
-            bucket: 'id:spotify'
-        });
-    }
+  artistsByLocation(loc, number) {
+    return this.Echonest.artists.search({
+      artist_location: loc,
+      results: number,
+      bucket: 'id:spotify'
+    });
+  }
 
-    getPlaylistSongs(loc) {
-        var self = this;
-        var artists = self.artistsByLocation(loc, 15);
-        return artists.then(function(artists) {
-            var artistIds = artists.map(function(artist) {
-                return artist.foreign_ids[0].foreign_id;
-            });
-            return self.q.all(artistIds.map(function(id) {
-                return self.Spotify.getArtistTopTracks(id.replace('spotify:artist:', ''), 'SE');
-            })).then(function(songs) {
-                return songs.map(function(song) {
-                    return song.tracks[0].id;
-                });
-            });
+  getPlaylistSongs(loc) {
+    var self = this;
+    var artists = self.artistsByLocation(loc, 15);
+    return artists.then(function(artists) {
+      var artistIds = artists.map(function(artist) {
+        return artist.foreign_ids[0].foreign_id;
+      });
+      return self.q.all(artistIds.map(function(id) {
+        return self.Spotify.getArtistTopTracks(id.replace('spotify:artist:', ''), 'SE');
+      })).then(function(songs) {
+        return songs.map(function(song) {
+          return song.tracks[0].id;
         });
-    }
+      });
+    });
+  }
 }
 
 EchonestService.$inject = ['Echonest', 'Spotify', '$q'];
